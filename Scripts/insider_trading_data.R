85d0fd4a612d042110ebc30108b9b226bc94b7ab
marcosmontoya03 <104536125+marcosmontoya03@users.noreply.github.com>
2025-12-01 18:19:23 -0500

    major updates to our get_insider_trading function

--- Scripts/insider_trading_data.R
+++ Scripts/insider_trading_data.R
@@ -5,78 +5,77 @@ library(janitor)
 library(stringr)
 
 
-get_insider_trading <- function(type){
+get_insider_trading <- function(){
   
-  tc_value <- if(type == "buy") {
-    "1"
-  } else if(type == "sale") {
-    "2" 
-  } else {
-    stop("Invalid type. Type buy or sale")
-  }
-
-  link <- paste0("https://finviz.com/insidertrading.ashx?tc=", tc_value)
-
-page <- read_html(link)
-
-df <- page %>% 
-  html_nodes(".text-right:nth-child(8) , .text-right:nth-child(7) , #insider-table .text-center+ .text-right , .text-left:nth-child(1) , #insider-table .text-center , .tabular-nums .tab-link , td:nth-child(1) .tab-link") %>% 
-  html_text() %>% 
-  as.data.frame()
-
-df <- df[-seq(13, nrow(df), by = 7), ] %>% 
-  as.data.frame() %>% 
-  rename(x = ".")
-
-df <- as.data.frame(matrix(df[,1], byrow=TRUE, ncol = 6))
-
-colnames(df) <- df[1, ]
-
-df <- df[-1, ] 
-
-df <- df %>% 
-  clean_names() %>% 
-  rename(total_value = value,
-         datetime = sec_form_4,
-         cost_share = cost) %>% 
-  mutate(date_part = word(datetime, 1, 2),
-         date_full = paste(date_part, "2025"),
-         date = as.Date(date_full, format = "%b %d %Y"),
-         time = word(datetime, 3, -1)) %>% 
-  select(-date_part, -date_full, -datetime)
+  results_list <- list()   # store each df here
   
+  counter <- 1
+  
+  for(tc_value in 1:2){
+    
+    link <- paste0("https://finviz.com/insidertrading.ashx?tc=", tc_value)
+    
+    page <- read_html(link)
+    
+    df <- page %>% 
+      html_nodes(".text-right:nth-child(8) , .text-right:nth-child(7) , 
+                 #insider-table .text-center+ .text-right , .text-left:nth-child(1) , 
+                 #insider-table .text-center , .tabular-nums .tab-link , td:nth-child(1) .tab-link") %>% 
+      html_text() %>% 
+      as.data.frame()
+    
+    df <- df[-seq(13, nrow(df), by = 7), ] %>% 
+      as.data.frame() %>% 
+      rename(x = ".")
+    
+    df <- as.data.frame(matrix(df[,1], byrow=TRUE, ncol = 6))
+    colnames(df) <- df[1, ]
+    df <- df[-1, ] 
 
-return(df)
-
+    df <- df %>% 
+      clean_names() %>% 
+      rename(total_value = value,
+             datetime = sec_form_4,
+             cost_share = cost) %>% 
+      mutate(date_part = word(datetime, 1, 2),
+             date_full = paste(date_part, "2025"),
+             date = as.Date(date_full, format = "%b %d %Y"),
+             time = word(datetime, 3, -1)) %>% 
+      select(-date_part, -date_full, -datetime) %>% 
+      filter(date == Sys.Date())
+    
+    results_list[[counter]] <- df
+    
+    counter <- counter + 1
+    
+    } 
+  
+  final_df <- bind_rows(results_list)
+  
+  return(final_df)
+  
 }
 
 
+  
 # DECEMBER 1 
-
-dec_1_sales <- get_insider_trading("sale")
-
-dec_1_buys <- get_insider_trading("buy")
-
-dec_1_buys <- dec_1_buys %>% 
-  filter(date == "2025-12-01")
-
-dec_1_trading <- rbind(dec_1_sales, dec_1_buys)
+dec_1 <- get_insider_trading()
+write.csv(dec_1, "dec_1_insider_trading.csv")
 
 
-write.csv(dec_1_trading, "dec_1_insider_trading.csv")
+# DECEMBER 2
+dec_2 <- get_insider_trading()
+write.csv(dec_2, "dec_2_insider_trading.csv")
 
+# DECEMBER 3
+dec_3 <- get_insider_trading()
+write.csv(dec_3, "dec_3_insider_trading.csv")
 
-# DECEMBER 1 
+# DECEMBER 4
+dec_4 <- get_insider_trading()
+write.csv(dec_4, "dec_4_insider_trading.csv")
 
-dec_2_sales <- get_insider_trading("sale")
-
-dec2_buys <- get_insider_trading("buy")
-
-dec_2_buys <- dec_1_buys %>% 
-  filter(date == "2025-12-01")
-
-dec_2_trading <- rbind(dec_1_sales, dec_1_buys)
-
-
-write.csv(dec_1_trading, "dec_2_insider_trading.csv")
+# DECEMBER 5
+dec_5 <- get_insider_trading()
+write.csv(dec_5, "dec_5_insider_trading.csv")
 
