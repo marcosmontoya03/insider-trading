de33fe36de282767b61edf803581c18615f14f72
Gavin Schilling <gavin_schilling@brown.edu>
2025-12-01 14:25:21 -0500

    Starting an outline after Monday's Class

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # insider-trading
+
+Final Project PHP 1560
+
+Gavin, Marcos
+
+# Purpose
+
+The purpose of this code is to measure the impact of insider trading on stock
+prices, as well as calculate the time 
+
+
+# Structure of Files
+
+The first file is load_insider_data.R
+
+The second file is load_stock_data.R
+
+The third file is triple_DiD.R
+
+The fourth file is user_input.R
+
+
+
+
+Google Dataset Search
+Dataverse
+UCI Dataset
+Kaggle (poor quality control)
---
+++ Scripts/Outline.R
@@ -0,0 +1,34 @@
+######## USER INPUT ##########
+
+one_stock <- function(ticker,
+                      date,
+                      pre_period, 
+                      post_period,
+                      sector){
+  #Confirm that ticker in our data
+  
+  #If not, throw an error
+  
+  
+  get_prices_data()
+  
+}
+
+whole_data_results <- funciton(){
+  
+}
+
+one_stock(ticker = "CNA", pretrend,  )
+
+whole_data_
+
+
+
+#' Get Price Data
+#' 
+#' @param type Specifies if the search is for a stock that experienced insider
+#' trading, if it is a sector control search,
+scrape_price_data <- function(ticker,
+                              pre_period,
+                              post_period,
+                              type)
