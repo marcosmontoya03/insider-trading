f2a651872e55967cb748eb17ef6228ea6209efb2
Gavin Schilling <gavin_schilling@brown.edu>
2025-12-03 13:32:12 -0500

    Setting up functions

--- Scripts/stock_price_data.R
+++ Scripts/stock_price_data.R
@@ -11,3 +11,19 @@ stocks <- read.csv("/Users/marco/Documents/GitHub/insider-trading/data/dec_1_ins
                    row.names = 1) %>% 
   select(ticker, date, time)
 
+
+
+
+#' Function to append the scraped data together
+#'  
+append_scraped_data <- function(){
+  
+  df_base = "data/dec_1_insider_trading.csv"
+  # Code to get the number of scraped files in the data folder
+  num_files <- 
+    
+    for(i in 2:num_files){
+      r_bind
+      
+    }
+}
---
+++ Scripts/user_interaction.R
@@ -0,0 +1,12 @@
+####### File to manage user interaction with the insider trading data and selecting
+# the functions and parameters to run
+
+#source("insider_trading_data.R")
+
+
+
+
+
+#' Function to ask user to pick the event they want vs whole data to do a DiD of
+#'  
+#'  
