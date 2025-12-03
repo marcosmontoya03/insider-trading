1f91600f770805c04124da4443866e25e4f1dd0b
marcosmontoya03 <104536125+marcosmontoya03@users.noreply.github.com>
2025-12-03 14:23:00 -0500

    major UPDATEs: prompt

--- Scripts/stock_price_data.R
+++ Scripts/stock_price_data.R
@@ -3,13 +3,59 @@ library(httr2)
 library(jsonlite)
 library(dplyr)
 library(tidyverse)
+library(lubridate)
 
 
 ### test ###
 
 stocks <- read.csv("/Users/marco/Documents/GitHub/insider-trading/data/dec_1_insider_trading.csv", 
-                   row.names = 1) %>% 
-  select(ticker, date, time)
+                   row.names = 1) %>%
+  group_by(ticker, time) %>% 
+  summarise(total = n())
+
+
+test <- dec_3 %>% 
+  mutate(time = parse_time(time)) %>% 
+  filter(time > hm("09:30"), time < hm("16:30")) %>% 
+  group_by(ticker, date) %>% 
+  mutate(total_events_in_day = n()) %>% 
+  ungroup() %>% 
+  group_by(ticker, datetime) %>% 
+  mutate(tot_event_same_time = n())
+
+  
+
+
+# GETTING MINUT BY MINUTE DATA 
+
+clean_insider_data <- function(df){
+  
+  threholds <- readline(prompt = " what threshold do you want? edplain what theshold is, and why it matters ")
+  
+  
+  df <- df %>%
+    mutate(time = parse_time(time)) %>% 
+    filter(time > hm("09:30"), time < hm("16:30")) %>% 
+    group_by(ticker, date) %>% 
+    mutate(total_events_in_day = n()) %>% 
+    ungroup() %>% 
+    group_by(ticker, datetime) %>% 
+    mutate(tot_event_same_time = n())
+    
+  # if(total in a day is same as total in datetime){
+  #   dont do anytign 
+  # }
+  # 
+  # else {
+  #   
+  #   if (they are close to each toher )
+  #     
+  #     user gets to pick if they wnat to collapse (based on threshold)
+  # }
+  
+}
+
+
 
 
 
