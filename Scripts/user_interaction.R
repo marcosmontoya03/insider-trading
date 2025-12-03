6f4ae25369f58662df2555408af9481510f461d1
Gavin Schilling <gavin_schilling@brown.edu>
2025-12-03 14:05:23 -0500

    Building out user input code

--- Scripts/user_interaction.R
+++ Scripts/user_interaction.R
@@ -2,6 +2,7 @@
 # the functions and parameters to run
 
 #source("insider_trading_data.R")
+source("stock_price_data.R")
 
 
 
@@ -9,4 +10,75 @@
 
 #' Function to ask user to pick the event they want vs whole data to do a DiD of
 #'  
-#'  
+#'
+user_interaction <- function(){
+  #Code to open something in the console
+  "Do you want to look at a specific ticker?"
+  
+  specific_event <- #user input
+    
+  if(specific_event == 1){
+    
+    selection <- event_selection(single = TRUE,
+                                 ticker = #user input)
+  }
+  else{
+    #Code to prompt user
+    "Do you want to look at a single day"
+    
+    single_day <- #user input
+      
+    if(single_day == 1){
+      while(TRUE){
+      #Code to prompt user
+      "How many days do you want to select?"
+      
+      start_date <- #user input
+      
+      end_date <- #user input
+      
+      if(end_date > start_date)  {
+        break
+      } 
+      else{
+        
+      }
+      }  
+      triple_did(date = )
+    }
+    triple_did(,selection)
+  }
+  
+}
+
+#' Function that will prompt the user to select the event and date and time
+#' they want if there are multiple instance of an event
+#' 
+#' @return A vector where the first index is the ticker and the second index is
+#' the date, and the third is the time, and the fourth is whether is was a buy,
+#' sale, or a proposed sale
+selection <- function(ticker){
+  
+  selection <- c(0,0,0,0)
+  selection[1] <- user_ticker
+  selection[2] <- user_date
+  selection[3] <- user_time
+  selection[4] <- user_direction
+  
+  return(selection)
+}
+
+
+triple_did <- function(){
+  ticker = NULL,
+  date = selection[2],
+  time = selection[3],
+  direction = selection[4]
+  
+  if(is.null(ticker)){
+    
+  } else{
+    
+  }
+  
+}
